c1ee7972c84d9a5f28305d05b6503f74e4e98171
sids278 <94206506+sids278@users.noreply.github.com>
2023-02-07 11:35:48 +0530

    Update app.js
    Functionallity to Mongoose

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express =require('express')
 const bodyParser=require('body-parser')
 const app=express()
-const MongoProducts=require('./Mongo')
+const MongoProducts=require('./Mongoose')
 
 
 app.use(bodyParser.json())
 
-app.get('/')
+app.get('/',MongoProducts.getProducts)
 app.post('/products',MongoProducts.createProduct)
 
 app.listen(8080,()=>{console.log("its lisetning")})
